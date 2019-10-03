274b3cc84b3401fb2399a02585a224c646cfe6b5
John <jmr16u@my.fsu.edu>
2019-10-03 17:01:07 -0400

    Creation of Jet.h
    Header file for Jet.h created with needed member data

---
+++ Jet.h
@@ -0,0 +1,29 @@
+//
+// Created by John Rawley on 10/3/19.
+//
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+
+private:
+    int numberOfEngines;
+
+public:
+    Jet();
+
+    explicit Jet(string brand, string model, string fuelType,
+                 int numberOfEngines = 1);
+
+    virtual ~Jet();
+    string getEngineSize();
+    void setNumberOfEngines(int numberOfEngines);
+    virtual double mileageEstimate(double time);
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_JET_H
