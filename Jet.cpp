395886bf34cf5de2139998dcf8d4a0caf0f74b8b
John <jmr16u@my.fsu.edu>
2019-10-03 17:12:24 -0400

    Jet.cpp Created
    Jet class functions have been defined, member data numberOfEngines is 
    default to 1 and if set to negitive number also set to 1. Milage bases 
    on fuel type and number of engines.

---
+++ Jet.cpp
@@ -0,0 +1,47 @@
+//
+// Created by John Rawley on 10/3/19.
+//
+
+#include "Jet.h"
+#include <cstdlib>
+
+Jet::Jet() {
+    numberOfEngines = 1;
+    setBrand("Custom");
+    setModel("VTx");
+}
+
+Jet::Jet(string brand, string model, string fuelType, int numberOfEngines) {
+    setBrand(brand);
+    setModel(model);
+    setFuelType(fuelType);
+    setNumberOfEngines(numberOfEngines);
+}
+
+Jet::~Jet() = default;
+
+int Jet::getNumberOfEngines() {
+    return numberOfEngines;
+}
+
+void Jet::setNumberOfEngines(int number) {
+    if(number >= 0 ){
+      numberOfEngines = number;
+    }
+    else
+      numberOfEngines = 1;
+}
+
+double Jet::mileageEstimate(double time) {
+    int temp = (rand() % 61) + 40;
+    double mileage = temp;
+    if (fuelType == "Rocket" && numberOfEngines > 2) {
+        mileage += mileage * 0.055;
+    }
+    return mileage;
+}
+
+string Jet::toString() {
+    return "-> Jet\n" + PoweredVehicle::toString() + "\n\tNumber of Engines: " +
+           getEngineSize();
+}
