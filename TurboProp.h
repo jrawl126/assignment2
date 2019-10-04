87b441e40c4173fdcafde78a04e46123e145979f
John <jmr16u@my.fsu.edu>
2019-10-04 09:15:37 -0400

    TurboProp Header File Created:
    Member variables include grossWeight, which is used to calculate milage

---
+++ TurboProp.h
@@ -0,0 +1,29 @@
+//
+// Created by John Rawley on 10/4/19.
+//
+
+#ifndef DRIVINGSIMULATOR_TURBOPROP_H
+#define DRIVINGSIMULATOR_TURBOPROP_H
+
+#include "PoweredVehicle.h"
+
+class Turboprop : public PoweredVehicle {
+
+private:
+    int grossWeight;
+
+public:
+    Turboprop();
+
+    explicit Turboprop(string brand, string model, string fuelType,
+                 int grossWeight);
+
+    virtual ~Turboprop();
+    int getGrossWeight();
+    void setGrossWeight(int grossWeight);
+    virtual double mileageEstimate(double time);
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_TURBOPROP_H
